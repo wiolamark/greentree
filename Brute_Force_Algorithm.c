1780af25823143e5dee3ce4b0f33fa8646da25d8
wiolamark <marcinkiewicz.wioletta@gmail.com>
2019-01-12 21:18:15 +0100

    First file

---
+++ Brute_Force_Algorithm.c
@@ -0,0 +1,30 @@
+// C program for Brute Force Searching algorithm
+#include <stdio.h>
+#include <string.h>
+
+void search(char* pat, char* txt)
+{
+	int M = strlen(pat);
+	int N = strlen(txt);
+
+
+	for (int i = 0; i <= N - M; i++) {
+		int j;
+
+
+		for (j = 0; j < M; j++)
+			if (txt[i + j] != pat[j])
+				break;
+
+		if (j == M)
+			printf("Pattern found at index %d \n", i);
+	}
+}
+
+int main()
+{
+	char txt[] = "ALGORYTMY I hhhhSTRUKTURY DANYCH";
+	char pat[] = "TUR";
+	search(pat, txt);
+	return 0;
+}
